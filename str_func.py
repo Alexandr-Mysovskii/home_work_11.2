21e23086565c60f51d4a6e42963f635940a907b4
Alexandr Mysovskii <sashka1836@mail.ru>
2023-08-07 16:22:30 +0300

    "add docstring and new second function"

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
-def title_word (string):
+def title_word(string):
+    """Возвращает все слова
+    введенной строки
+    с заглавной буквы
+    """
+    return string.title()
+
+def title_word_second(string):
+    """Возвращает все слова
+    с заглавной буквой"""
     return string.title()
